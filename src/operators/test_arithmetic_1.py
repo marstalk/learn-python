24d5e34730da672d11ddfc3edb1e9a64327dfd62
Louisl <louisl@synnex.com>
2019-06-27 09:08:20 +0800

    add test_arithmetic_1.py

---
+++ src/operators/test_arithmetic_1.py
@@ -0,0 +1,11 @@
+"""Arithmetic operators
+@see: http://www.w3schools.com/python/python_operators.asp
+
+Arithmetic operators are used with numeric values to perform common mathematical operations
+"""
+
+def test_arithmetic_operators():
+    """Arithmetic operators"""
+
+    #Addiction
+    assert 5 + 3 = 8
